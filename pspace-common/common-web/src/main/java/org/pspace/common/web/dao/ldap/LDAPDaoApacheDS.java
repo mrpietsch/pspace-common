2f0e5f850f96cdc75acd6b8bd8100cd8d1a4ca09
Martin Pietsch <martin@p-space.org>
2013-02-17 17:24:09 +0100

    got apacheds 2.0.0 working except for some warnings

--- pspace-common/common-web/src/main/java/org/pspace/common/web/dao/ldap/LDAPDaoApacheDS.java
+++ pspace-common/common-web/src/main/java/org/pspace/common/web/dao/ldap/LDAPDaoApacheDS.java
@@ -1,26 +1,15 @@
 package org.pspace.common.web.dao.ldap;
 
-import org.apache.directory.server.constants.ServerDNConstants;
-import org.apache.directory.server.core.DefaultDirectoryService;
-import org.apache.directory.server.core.DirectoryService;
+import org.apache.directory.api.ldap.model.entry.Entry;
+import org.apache.directory.api.ldap.model.name.Dn;
+import org.apache.directory.server.core.annotations.CreateDS;
+import org.apache.directory.server.core.api.DirectoryService;
+import org.apache.directory.server.core.api.partition.Partition;
 import org.apache.directory.server.core.factory.DefaultDirectoryServiceFactory;
-import org.apache.directory.server.core.factory.DirectoryServiceFactory;
-import org.apache.directory.server.core.partition.Partition;
-import org.apache.directory.server.core.partition.impl.btree.jdbm.JdbmIndex;
+import org.apache.directory.server.core.factory.PartitionFactory;
 import org.apache.directory.server.core.partition.impl.btree.jdbm.JdbmPartition;
-import org.apache.directory.server.core.partition.ldif.LdifPartition;
-import org.apache.directory.server.core.schema.SchemaPartition;
 import org.apache.directory.server.ldap.LdapServer;
 import org.apache.directory.server.protocol.shared.transport.TcpTransport;
-import org.apache.directory.server.xdbm.Index;
-import org.apache.directory.shared.ldap.entry.ServerEntry;
-import org.apache.directory.shared.ldap.name.DN;
-import org.apache.directory.shared.ldap.schema.SchemaManager;
-import org.apache.directory.shared.ldap.schema.ldif.extractor.SchemaLdifExtractor;
-import org.apache.directory.shared.ldap.schema.ldif.extractor.impl.DefaultSchemaLdifExtractor;
-import org.apache.directory.shared.ldap.schema.loader.ldif.LdifSchemaLoader;
-import org.apache.directory.shared.ldap.schema.manager.impl.DefaultSchemaManager;
-import org.apache.directory.shared.ldap.schema.registries.SchemaLoader;
 import org.pspace.common.api.Person;
 import org.pspace.common.web.dao.LDAPDao;
 import org.pspace.common.web.mvc.PhoneEditor;
@@ -31,15 +20,11 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.StringUtils;
 import sun.misc.BASE64Encoder;
 
-import javax.naming.InvalidNameException;
-import javax.naming.NamingException;
 import java.io.File;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
 
 /**
  * Created at 08.12.11 - 07:30
@@ -75,80 +60,6 @@ public class LDAPDaoApacheDS implements LDAPDao, InitializingBean, DisposableBea
         ldapServer.stop();
     }
 
-    /**
-     * Add a new partition to the server
-     *
-     * @param partitionId The partition Id
-     * @param partitionDn The partition DN
-     * @return The newly added partition
-     * @throws Exception If the partition can't be added
-     */
-    private JdbmPartition addPartition(String partitionId, String partitionDn) throws Exception {
-        // Create a new partition named 'foo'.
-        JdbmPartition partition = new JdbmPartition();
-        partition.setId(partitionId);
-        partition.setPartitionDir(new File(directoryService.getWorkingDirectory(), partitionId));
-        partition.setSuffix(partitionDn);
-        directoryService.addPartition(partition);
-
-        return partition;
-    }
-
-
-    /**
-     * Add a new set of index on the given attributes
-     *
-     * @param partition The partition on which we want to add index
-     * @param attrs     The list of attributes to index
-     */
-    private void addIndex(JdbmPartition partition, String... attrs) {
-        // Index some attributes on the apache partition
-        HashSet<Index<?, ServerEntry, Long>> indexedAttributes = new HashSet<Index<?, ServerEntry, Long>>();
-
-        for (String attribute : attrs) {
-            indexedAttributes.add(new JdbmIndex<String, ServerEntry>(attribute));
-        }
-
-        partition.setIndexedAttributes(indexedAttributes);
-    }
-
-    /**
-     * initialize the schema manager and add the schema partition to diectory service
-     *
-     * @throws Exception if the schema LDIF files are not found on the classpath
-     */
-    private void initSchemaPartition() throws Exception {
-        SchemaPartition schemaPartition = directoryService.getSchemaService().getSchemaPartition();
-
-        // Init the LdifPartition
-        LdifPartition ldifPartition = new LdifPartition();
-        String workingDirectory = directoryService.getWorkingDirectory().getPath();
-        ldifPartition.setWorkingDirectory(workingDirectory + "/schema");
-
-        // Extract the schema on disk (a brand new one) and load the registries
-        File schemaRepository = new File(workingDirectory, "schema");
-        SchemaLdifExtractor extractor = new DefaultSchemaLdifExtractor(new File(workingDirectory));
-        extractor.extractOrCopy(true);
-
-        schemaPartition.setWrappedPartition(ldifPartition);
-
-        SchemaLoader loader = new LdifSchemaLoader(schemaRepository);
-        SchemaManager schemaManager = new DefaultSchemaManager(loader);
-        directoryService.setSchemaManager(schemaManager);
-
-        // We have to load the schema now, otherwise we won't be able
-        // to initialize the Partitions, as we won't be able to parse
-        // and normalize their suffix DN
-        schemaManager.loadAllEnabled();
-
-        schemaPartition.setSchemaManager(schemaManager);
-
-        List<Throwable> errors = schemaManager.getErrors();
-
-        if (!errors.isEmpty()) {
-            throw new Exception("Schema load failed : " + errors);
-        }
-    }
 
 
     public void afterPropertiesSet() throws Exception {
@@ -162,34 +73,29 @@ public class LDAPDaoApacheDS implements LDAPDao, InitializingBean, DisposableBea
             // example
             String rootDc = rootDnParts[0];
 
-            directoryService = new DefaultDirectoryService();
-            directoryService.setShutdownHookEnabled(true);
-            directoryService.setWorkingDirectory(getTempDir());
+            DefaultDirectoryServiceFactory factory = new DefaultDirectoryServiceFactory();
+            factory.init("Test");
+            directoryService = factory.getDirectoryService();
+            directoryService.setAllowAnonymousAccess(true);
+            directoryService.setAccessControlEnabled(false);
 
-            // first load the schema
-            initSchemaPartition();
+            PartitionFactory partitionFactory = factory.getPartitionFactory();
 
-            // then the system partition -- this is a MANDATORY partition
-            Partition systemPartition = addPartition("system", ServerDNConstants.SYSTEM_DN);
-            directoryService.setSystemPartition(systemPartition);
+            Partition partition = partitionFactory.createPartition(directoryService.getSchemaManager(), rootDc, rootDn, 0, getTempDir());
+//
+//            Partition partition = new JdbmPartition(directoryService.getSchemaManager());
+//            partition.setId(rootDc);
+//            partition.setSuffixDn(new Dn(rootDn));
+            directoryService.addPartition(partition);
 
-            // Disable the ChangeLog system
-            directoryService.getChangeLog().setEnabled(false);
-            directoryService.setDenormalizeOpAttrsEnabled(true);
-
-            // Now we can create as many partitions as we need
-            JdbmPartition partition = addPartition(rootDc, rootDn);
-
-            // Index some attributes on the apache partition
-            addIndex(partition, "objectClass", "ou", "uid");
 
             // start the service
             directoryService.startup();
 
             // create the root entry
             if (!directoryService.getAdminSession().exists(partition.getSuffixDn())) {
-                DN dnApache = new DN(rootDn);
-                ServerEntry entryApache = directoryService.newEntry(dnApache);
+                Dn dnApache = new Dn(rootDn);
+                Entry entryApache = directoryService.newEntry(dnApache);
                 entryApache.add("objectClass", "top", "domain", "extensibleObject");
                 entryApache.add("dc", rootDc);
                 directoryService.getAdminSession().add(entryApache);
@@ -199,7 +105,6 @@ public class LDAPDaoApacheDS implements LDAPDao, InitializingBean, DisposableBea
             ldapServer = new LdapServer();
             ldapServer.setDirectoryService(directoryService);
             ldapServer.setTransports(new TcpTransport(port));
-            ldapServer.setAllowAnonymousAccess(true);
             ldapServer.start();
 
         } catch (Exception e) {
@@ -229,9 +134,9 @@ public class LDAPDaoApacheDS implements LDAPDao, InitializingBean, DisposableBea
     public void updatePerson(Person person) {
         try {
             String trim = person.getFullName().trim();
-            DN memberDn = new DN("cn=" + trim + "," + rootDn);
+            Dn memberDn = new Dn("cn=" + trim + "," + rootDn);
 
-            ServerEntry memberEntry = directoryService.newEntry(memberDn);
+            Entry memberEntry = directoryService.newEntry(memberDn);
             memberEntry.add("objectClass", "top", "person", "inetOrgPerson", "organizationalPerson");
             memberEntry.add("employeeNumber", "" + person.getId());
             memberEntry.add("cn", trim);
@@ -258,10 +163,6 @@ public class LDAPDaoApacheDS implements LDAPDao, InitializingBean, DisposableBea
             if (person.getCity() != null && !person.getCity().isEmpty()) memberEntry.add("l", person.getCity());
             if (person.getPostalCode() != null && !person.getPostalCode().isEmpty()) memberEntry.add("postalCode", person.getPostalCode());
             directoryService.getAdminSession().add(memberEntry);
-        } catch (InvalidNameException e) {
-            throw new RuntimeException(e);
-        } catch (NamingException e) {
-            throw new RuntimeException(e);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
